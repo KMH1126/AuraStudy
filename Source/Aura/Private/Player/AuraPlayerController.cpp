2124e0b25f775ce8ecd4a621b3af2b32b6e24de9
KKMMHH <2966218194@qq.com>
2026-01-19 22:07:14 +0800

    playerController

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true; 
+}
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); 
+	check(AuraContext); 
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());  
+	check(Subsystem); 
+	Subsystem->AddMappingContext(AuraContext, 0); //填入映射map ； 
+
+	bShowMouseCursor = true; 
+	DefaultMouseCursor = EMouseCursor::Default; 
+
+	FInputModeGameAndUI InputModeData; 
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false); 
+	SetInputMode(InputModeData); 
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent); 
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move); 
+
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>(); //这里获取的是键盘（WSAD）或手柄左摇杆的原始值。//InputAxisVector.Y：通常代表 前/后（W/S）。//InputAxisVector.X：通常代表 左/右（A/D）。
+	const FRotator Rotation = GetControlRotation();						//这是 控制器（Controller） 的旋转，通常等同于 摄像机（Camera） 的朝向。		
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);					
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X); 
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControllPawn = GetPawn<APawn>())
+	{
+		ControllPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y); 
+		ControllPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+
+	}
+
+}
